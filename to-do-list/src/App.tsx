5f129aef20661010d5440fca17d3c0d104ed3268
fnixsik <fnixsikMen@mail.ru>
2023-05-30 21:49:17 +0600

    add few items

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,26 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './components/block/box';
+import {v1} from 'uuid';
 
-const boxName = {}
-let boxValue = [
-  {id : 1, title: 'js', isDone: true},
-  {id : 2, title: 'css', isDone: false},
-  {id : 3, title: 'React', isDone: true},
-]
-
-let boxValue1 = [
-  {id : 1, title: 'ang', isDone: false},
-  {id : 2, title: 'kz', isDone: false},
-  {id : 3, title: 'turk', isDone: true},
-]
+export type filterType = 'all' | 'half' | 'last'
 
 function App() {
-  return (
+
+  let [increState, setState] = useState(
+    [
+    {id : v1(), title: 'js', isDone: true},
+    {id : v1(), title: 'css', isDone: false},
+    {id : v1(), title: 'React', isDone: true},
+  ]
+)
+  let [increFilter, setFilter] = useState<filterType>('all')
+
+  function remuveBoxValue (id : string){
+    let setNewDate = increState.filter(g=> g.id !== id)
+    setState(setNewDate);
+  }
+
+  function filterStateCall (value : filterType){
+    setFilter(value);
+  }
+
+  function addTableLine (title : string){
+    let createNewLine = {id : v1(), title: title, isDone: false}
+    let pushState = [createNewLine, ...increState]
+    setState(pushState);
+  }
+
+
+  let filterState = increState;
+  if(increFilter === 'half'){
+    filterState = increState.filter(u => u.isDone === true)
+  }
+  else if (increFilter === 'last'){
+    filterState = increState.filter(u => u.isDone === false)
+  }
+
+
+  return(
     <div className="App">
-      <Box title = 'What you need lernd' boxValue = {boxValue}/>
-      <Box title = 'how many languages know ?' boxValue = {boxValue1}/>
+      <Box title = 'What you need lernd' 
+      increState = {filterState} 
+      remuve = {remuveBoxValue}
+      callfilter = {filterStateCall}
+      callAddLine = {addTableLine}
+      />
     </div>
-  );
+  )
+  
 }
+
 
 export default App;
--- to-do-list/src/components/block/box.tsx
+++ to-do-list/src/components/block/box.tsx
@@ -1,31 +1,48 @@
+import { useState } from "react";
+import { filterType } from "../../App";
+
+
 type TaskType = {
-  id : number;
+  id : string;
   title : string;
   isDone : boolean; 
 }
 
 type PropsType = {
   title : string;
-  boxValue : Array<TaskType>;
+  increState : Array<TaskType>;
+  remuve : (id: string)=> void;
+  callfilter: (value: filterType)=> void;
+  callAddLine: (title : string)=> void;
 }
 
 function Box(props: PropsType ) {
+
+const [increvalue, setvalue] = useState('');
+
   return (
     <div>
       <h3>{props.title}</h3>
       <div>
-        <input type="text" />
-        <button>+</button>
+        <input type="text" value={increvalue} onChange={(e)=>{setvalue(e.currentTarget.value)}} />
+        <button onClick={()=>{props.callAddLine(increvalue)}}>+</button>
       </div>
       <ul>
         {
-          props.boxValue.map((i)=> {
-           return <li><input type="checkbox" checked={i.isDone}/><span>{i.title}</span></li>
+          props.increState.map((i)=> {
+           return <li><input type="checkbox" checked={i.isDone}/><span>{i.title}</span>
+           <button onClick={()=> props.remuve(i.id)}>Удалить</button>
+           </li>
           })
         }
       </ul>
+      <div>
+        <button onClick={()=>{props.callfilter('all')}}>All</button>
+        <button onClick={()=>{props.callfilter('half')}}>Active</button>
+        <button onClick={()=>{props.callfilter('last')}}>Complated</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Box;
